b31e061e570aa76ebf70cc9ad33f8495f3c9168c
Amir Motavas <amir.mts79@gmail.com>
2026-06-23 18:04:15 +0430

    feat(orders): implement order data layer and module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import {DatabaseModule} from '@app/common'
+import * as Joi from 'joi'
+import { MongooseModule } from '@nestjs/mongoose';
+import {Order,OrderSchema} from './schema/order.schema'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal:true,
+    validationSchema: Joi.object({
+      MONGO_URI: Joi.string().required(),
+    }),
+    envFilePath: './apps/orders/.env',
+  }),
+  DatabaseModule,
+  MongooseModule.forFeature([{name:Order.name,schema:OrderSchema}])
+],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
